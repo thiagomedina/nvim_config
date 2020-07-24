f8574049ab08878ef0a58747ad229ec4d841fb58
thiagomedina <thiagomedina001@gmail.com>
2020-07-24 19:55:57 -0300

    add mixin scss

--- frontend/src/view/home/index.js
+++ frontend/src/view/home/index.js
@@ -1,11 +1,11 @@
 import React from "react";
 import Navbar from "../../components/navbar";
-import { background, teste } from "./home.module.scss";
+import { background } from "./home.module.scss";
 const Home = () => {
   return (
     <>
       <Navbar />
-      <div className={`${background} ${teste}`}>
+      <div className={`${background}`}>
         <h2>Faça seu currículo de forma rápida e simples</h2>
         <p>lorem ipsu</p>
         <button>começar</button>
