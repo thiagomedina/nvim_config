f99185e0988e5f6c479aa0b120420e10a6b64560
thiagomedina <thiagomedina001@gmail.com>
2020-07-30 21:27:59 -0300

    refactoring

--- backend-api/index.js
+++ backend-api/index.js
@@ -1,8 +1,22 @@
-const app = require('./server/config/express');
-const config = require('./server/config/env');
+import express from "express";
+import bodyParser, { urlencoded } from "body-parser";
 
-app.listen(config.portApi, () => {
+const app = express();
 
-    `Server started on port ${config.portApi}(${config.env})`
-});
-module.exports = app;
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: false,
+  })
+);
+
+app.listen(3000)
+
+// const app = require('./server/config/express');
+// const config = require('./server/config/env');
+
+// app.listen(config.portApi, () => {
+
+//     `Server started on port ${config.portApi}(${config.env})`
+// });
+// module.exports = app;
