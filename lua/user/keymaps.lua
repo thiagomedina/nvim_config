57cd7ca6bbc7bc2c34843cf2b3c1c2ea050e6d83
thiagomedina <thiagomedina001@gmail.com>
2022-01-12 10:05:57 -0300

    added keymap

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -25,7 +25,8 @@ keymap("n", "<C-j>", "<C-w>j", opts)
 keymap("n", "<C-k>", "<C-w>k", opts)
 keymap("n", "<C-l>", "<C-w>l", opts)
 -- Formatting
-keymap("n", "<C-i>", ":lua vim.lsp.buf.formatting_sync()<CR>", opts)
+keymap("n", '"', 'ciw""<ESC>P', opts)
+keymap("n", "'", "ciw''<ESC>P", opts)
 -- Resize with arrows
 keymap("n", "<C-Up>", ":resize -2<CR>", opts)
 keymap("n", "<C-Down>", ":resize +2<CR>", opts)
