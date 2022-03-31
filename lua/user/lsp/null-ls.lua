92e80a2b8b83badb0895cfe23b986faae4fe7589
thiagomedina <thiagomedina001@gmail.com>
2022-03-31 12:35:21 -0300

    config formatting

--- lua/user/lsp/null-ls.lua
+++ lua/user/lsp/null-ls.lua
@@ -11,9 +11,13 @@ local diagnostics = null_ls.builtins.diagnostics
 null_ls.setup({
 	debug = false,
 	sources = {
-		formatting.prettier,
+		formatting.prettier.with({extra_args = {'--print-width=120'}}),
 		formatting.black.with({ extra_args = { "--fast" } }),
 		formatting.stylua,
-    -- diagnostics.flake8
+    formatting.lua_format.with({
+      extra_args = {
+        '--column-limit=120'
+      }
+    })
 	},
 })
