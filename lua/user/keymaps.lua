149ee608e6f95055a0075c8b217924db77906256
Thiago-medina <thiagomedina001@gmail.com>
2022-10-27 18:06:35 -0300

    keymap

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -64,6 +64,9 @@ keymap("x", "K", ":move '<-2<CR>gv-gv", opts)
 keymap("x", "<A-j>", ":move '>+1<CR>gv-gv", opts)
 keymap("x", "<A-k>", ":move '<-2<CR>gv-gv", opts)
 
+--replace
+keymap("n", "<Leader>r", ":%s///g<Left><Left>", opts)
+
 -- Terminal --
 -- Better terminal navigation
 -- keymap("t", "<C-h>", "<C-\\><C-N><C-w>h", term_opts)
